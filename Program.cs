155ccffb00ddd29efc34d85338206933f6cb14d9
Evgen <you@example.com>
2023-01-29 21:58:51 +0300

    создание масива

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string [] Arrey(){
+    Console.WriteLine($" Введите размер масива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string [] arr = new string [size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите элемент масива {i} ");
+       arr[i]  = Console.ReadLine();
+    }
+    return arr;
+}
